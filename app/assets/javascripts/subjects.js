5a4886e097403f845369b1a35166b2098f9f69b3
meera <meerasahebshaik12@gmail.com>
2016-12-02 10:38:49 +0530

    contentportalrails

---
+++ app/assets/javascripts/subjects.js
@@ -0,0 +1,23 @@
+//  $(document).ready(function(){
+//  	$("#add-button").click(function(){
+        
+//    $("#render-form").removeClass("render")
+   
+//  });
+// })
+
+
+
+//  $(document).ready(function(){
+//   $( ".collapsible-header" ).click(function() {
+//       $(".more",this).toggle()
+//       $(".less", this).toggle()
+//   });
+// });
+
+ $('#click_advance').click(function(){
+$('#display_advance').toggle('1000');
+$(this).html('<i class="icon-circle-arrow-up"></i> Advanced search');
+});
+
+
---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,50 @@
+class SubjectsController < ApplicationController
+  def index
+   @subjects = Subject.all
+   @subject = Subject.new
+
+  end
+
+  def new
+    end
+
+  def show
+     @subject = Subject.find(params[:id])
+  end
+
+  def create
+     @subject = Subject.new(subject_params)
+     if @subject.save
+      redirect_to subjects_url
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+     @subject = Subject.find(params[:id])
+  end
+
+  def update
+     @subject = Subject.find(params[:id])
+     if @subject.update(subject_params)
+      redirect_to @subject
+    else
+       render :edit 
+    
+  end
+  end
+
+  def destroy
+     @subject = Subject.find(params[:id])
+     @subject.destroy
+     redirect_to subjects_url
+
+  end
+  private
+  def subject_params
+    params.require(:subject).permit(:name)
+  end
+
+end
